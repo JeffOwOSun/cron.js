04c168cc73f1ab365de04e946c0e3f792eda1d9f
Yushi Sun <sunyushi9546@gmail.com>
2016-09-01 14:02:21 +0800

    add more logs to test cases

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ var cron = require('./cron.js');
 var moment = require('moment-timezone');
 
 var tg = cron.TimeGroup(cron.exampleRules);
+console.log('rules:', cron.exampleRules);
+console.log('start stop:', cron.exampleStart, cron.exampleStop);
 console.log(tg.enumerate(cron.exampleStart, cron.exampleStop).map(function(date){
   return moment(date).tz('Asia/Hong_Kong').format('YYYY-MM-DD HH:mm:ss z');
 }));
